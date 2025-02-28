d033862521a0204e4e7500dcff2eca5f14e07fb3
pedro <pedrolsantos390@gmail.com>
2025-02-28 14:04:20 -0300

    ajustando material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/routes";
+import { ThemeProvider } from "@emotion/react";
+import { Light } from "@mui/icons-material";
+
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
+  <ThemeProvider theme={Light}>
+    <RouterProvider router={router} />
+  </ThemeProvider>
+)
 }
 
 export default App
